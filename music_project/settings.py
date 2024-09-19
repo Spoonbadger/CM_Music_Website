ba56262032ad4e9786f39e28095fe764ccea32fb
Spoonbadger <craig.adam.morley@gmail.com>
2024-09-19 16:57:45 +0100

    File: email info deleted.

--- music_project/settings.py
+++ music_project/settings.py
@@ -35,8 +35,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'craig.adam.morley@gmail.com'
-EMAIL_HOST_PASSWORD = 'vpsa hzwf airs trvd'
 
 
 # Application definition
